90792bf24a2316249235bb741baa0017fac89326
pradeepgowda1999 <158177273+pradeepgowda1999@users.noreply.github.com>
2026-01-03 15:59:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Good Morning All. Welcome to Jenkins and Git Online Training!");
+        System.out.println("Good Morning All. Welcome to Jenkins CICD and Git SCM Online Trainings!");
 
     }
 
