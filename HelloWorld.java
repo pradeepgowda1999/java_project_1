7cbe104de4489a6b8b37c4dce58534bcfbf0f49e
pradeepgowda1999 <158177273+pradeepgowda1999@users.noreply.github.com>
2026-01-03 12:46:07 +0530

    Update welcome message in HelloWorld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Jenkins and Git Online Training!");
+        System.out.println("Good Morning All. Welcome to Jenkins and Git Online Training!");
 
     }
 
